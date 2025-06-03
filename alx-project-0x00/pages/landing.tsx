387d0b0d42bf2301aa03d671bc70f155e5ff7024
thekuu <127185902+thekuu@users.noreply.github.com>
2025-06-04 01:33:48 +0300

    Advanced Task Updated

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -11,21 +11,21 @@ const Landing: React.FC = () => {
       <div className="space-x-2">
         <Button title="Small - sm" styles="text-sm rounded-sm" />
         <Button title="Small - md" styles="text-sm rounded-md" />
-        <Button title="Small - full" styles="text-sm rounded-full" />
+        <Button title="Small - full" styles="text-sm rounded-lg" />
       </div>
 
       {/* Medium Buttons */}
       <div className="space-x-2">
         <Button title="Medium - sm" styles="text-base rounded-sm" />
         <Button title="Medium - md" styles="text-base rounded-md" />
-        <Button title="Medium - full" styles="text-base rounded-full" />
+        <Button title="Medium - full" styles="text-base rounded-lg" />
       </div>
 
       {/* Large Buttons */}
       <div className="space-x-2">
         <Button title="Large - sm" styles="text-lg rounded-sm" />
         <Button title="Large - md" styles="text-lg rounded-md" />
-        <Button title="Large - full" styles="text-lg rounded-full" />
+        <Button title="Large - full" styles="text-lg rounded-lg" />
       </div>
         </div>
     )
